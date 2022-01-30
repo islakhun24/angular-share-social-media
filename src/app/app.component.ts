f0835c9362a13f5178192538ee5e6d03592d84ce
islakhun <51625871+islakhun24@users.noreply.github.com>
2022-01-30 23:32:16 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,33 +15,34 @@ export class AppComponent {
   faTwitterSquare = faTwitterSquare;
   faTiktok = faTiktok;
   faLine = faLine;
+  url = 'https://app-uat.doku.com/retail/merchant/jasmine-catering-gen1/Baju-bayi-10a51e523bf14655';
   onClickFacebook(){
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('https://spacenews.com/caci-ramping-up-production-of-optical-terminals-for-low-earth-orbit-satellites'));
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + this.url);
     return false
   };
 
   onClickInstagram(){
-    window.open('http://instagram.com/sharer.php?u=' +'add custom text here');
+    window.open('http://instagram.com/p/');
     return false;
   };
 
   onClickWhatsapp(){
-    window.open('whatsapp://send?text='+  encodeURIComponent('https://spacenews.com/caci-ramping-up-production-of-optical-terminals-for-low-earth-orbit-satellites'));
+    window.open('whatsapp://send?text='+  this.url);
     return false;
   };
 
   onClickTwitter(){
-    window.open('http://twitter.com/share?text='+ encodeURIComponent('https://spacenews.com/caci-ramping-up-production-of-optical-terminals-for-low-earth-orbit-satellites'));
+    window.open('http://twitter.com/share?text='+ this.url);
     return false
   }
 
   onClickTiktok(){
-    window.open('http://twitter.com/share?text='+ encodeURIComponent('https://spacenews.com/caci-ramping-up-production-of-optical-terminals-for-low-earth-orbit-satellites'));
+    window.open('http://twitter.com/share?text='+ this.url);
     return false
   }
 
   onClickLine(){
-    window.open('https://social-plugins.line.me/lineit/share?url='+encodeURIComponent('https://spacenews.com/caci-ramping-up-production-of-optical-terminals-for-low-earth-orbit-satellites'));
+    window.open('https://social-plugins.line.me/lineit/share?url='+ this.url);
     return false;
   }
 

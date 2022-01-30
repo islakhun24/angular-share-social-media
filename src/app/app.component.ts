9c782d9186ee0e54dde55058b96007845bd284ef
islakhun <51625871+islakhun24@users.noreply.github.com>
2022-01-31 01:00:33 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,11 +26,6 @@ export class AppComponent {
   }
   ngOnInit(){
     this.metaTagService.addTags([
-  //     <meta property="og:url"                content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
-  // <meta property="og:type"               content="article" />
-  // <meta property="og:title"              content="When Great Minds Don’t Think Alike" />
-  // <meta property="og:description"        content="How much does culture influence creative thinking?" />
-  // <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
       {
         property: 'og:url',
         content: 'https://angular-share-social-media.vercel.app'

771ffac7999426f9c64d636c79451b143289c5c5
islakhun <51625871+islakhun24@users.noreply.github.com>
2022-01-31 00:42:17 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,6 +50,10 @@ export class AppComponent {
       {
         property: 'og:image',
         content: 'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg'
+      },
+      {
+        name: 'twitter:image',
+        content: 'http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg'
       }
     ]);
   }

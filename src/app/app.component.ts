68e5274b9842df39c7c5959f2cee48dedd062511
islakhun24 <islakhunnurdzaki24@gmail.com>
2022-02-16 01:52:36 +0700

    asdads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,9 +84,7 @@ export class AppComponent {
     }
   }
   onClickFacebook(){
-    const windowObj = window.open();
-    windowObj!.document.head.innerHTML='<meta property="og:title" content="The Rock"/><meta property="og:type" content="movie"/><meta property="og:url" content="http://www.imdb.com/title/tt0117500/"/><meta property="og:image" content="http://ia.media-imdb.com/rock.jpg"/><meta property="og:site_name" content="IMDb"/><meta property="fb:admins" content="USER_ID"/><meta property="og:description"      content="A group of U.S. Marines, under command of               a renegade general, take over Alcatraz and               threaten San Francisco Bay with biological               weapons."/>'
-    window.open('https://www.facebook.com/sharer/sharer.php?u=' + this.url);
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + this.url)!.document.head.innerHTML='<meta property="og:title" content="The Rock"/><meta property="og:type" content="movie"/><meta property="og:url" content="http://www.imdb.com/title/tt0117500/"/><meta property="og:image" content="http://ia.media-imdb.com/rock.jpg"/><meta property="og:site_name" content="IMDb"/><meta property="fb:admins" content="USER_ID"/><meta property="og:description"      content="A group of U.S. Marines, under command of               a renegade general, take over Alcatraz and               threaten San Francisco Bay with biological               weapons."/>';
     return false
   };
 
